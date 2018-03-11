0e7aa8e52d8873ca3f9b58e9153e55095eb5f18d
Artur Juraszek <asgavar@gmail.com>
2018-03-11 20:09:57 +0100

    PO -> singleton

---
+++ semestr_2_lato_2017_18/progob/lista03/SingletonDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Singleton;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sunday, March 11, 2018 15:30:11
+            int workshopsTime = 1520782211;
+            // Sun, 11 Mar 2018 18:54:59 GMT
+            int outOfWorkshopsTime = 1520794499;
+
+            var instance0 = TimeNTon.GetInstance(outOfWorkshopsTime);
+            var instance1 = TimeNTon.GetInstance(outOfWorkshopsTime);
+
+            Console.WriteLine(
+                "Dwie instancje utworzone poza godzinami pracowni:"
+            );
+            Console.WriteLine($"Instancja #0: {instance0}");
+            Console.WriteLine($"Instancja #1: {instance1}");
+
+            var instance2 = TimeNTon.GetInstance(workshopsTime);
+            var instance3 = TimeNTon.GetInstance(workshopsTime);
+            var instance4 = TimeNTon.GetInstance(workshopsTime);
+            var instance5 = TimeNTon.GetInstance(workshopsTime);
+
+            Console.WriteLine(
+                "Cztery instancje utworzone w czasie pracowni:"
+            );
+            Console.WriteLine($"Instancja #2: {instance2}");
+            Console.WriteLine($"Instancja #3: {instance3}");
+            Console.WriteLine($"Instancja #4: {instance4}");
+            Console.WriteLine($"Instancja #5: {instance5}");
+        }
+    }
+}
