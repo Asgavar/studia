13fa4c7ad621a646c10a827b7fd6f258705a2233
Artur Juraszek <asgavar@gmail.com>
2018-03-23 19:54:30 +0100

    po: IEnumerable

---
+++ semestr_2_lato_2017_18/progob/lista04/EnumerablePrimes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EnumerablePrimes
+{
+    class Program
+    {
+        /// <summary>
+        ///   Wypisanych zostaje 101 pierwszych liczb pierwszych.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            foreach (var prime in new PrimeCollection())
+            {
+                Console.WriteLine(prime);
+                ++counter;
+                if (counter >= 100)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
