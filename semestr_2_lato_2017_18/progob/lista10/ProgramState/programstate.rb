eb0775ac6ec96e310cecbd0065f4434e5582b52f
Artur Juraszek <artur@juraszek.xyz>
2018-05-10 17:17:52 +0200

    po: działające zapisywanie _wszystkich_ obiektów w Rubym

--- semestr_2_lato_2017_18/progob/lista10/ProgramState/programstate.rb
+++ semestr_2_lato_2017_18/progob/lista10/ProgramState/programstate.rb
@@ -12,10 +12,11 @@ class Snapshot
     current_index = 0
     objects_to_save = []
     ObjectSpace.each_object { |o|
-      unless current_index < 10
+      unless current_index < 100
         break
       end
       objects_to_save.push(o)
+      current_index += 1
     }
     return format_object.save(objects_to_save)
   end
@@ -27,26 +28,23 @@ class FormatHTML
     list_body = objects.map { |o|
       object_to_li(o)
     }.join("\n")
-    "<html>" + "<ol>" + list_body + "</ol>" + "</html>"
+    '<html>' + '<ol>' + list_body + '</ol>' + '</html>'
   end
 
   def parse(s)
     parsed_pairs = []
     wo_unnecessary_tags = s[9, s.length - 9]
     wo_unnecessary_tags.each_line { |line|
-      puts line
       parsed_pairs.push([
-        #line.match(/<h3>(.*)<\/h3>/),
         line.match(/<h3>(.*)<\/h3>/).captures[0],
-        #line.match(/<\/h3>(.*)<\/li>/),
         line.match(/<\/h3>(.*)<\/li>/).captures[0]
       ])
     }
-    return parsed_pairs
+    parsed_pairs
   end
 
   def object_to_li(o)
-    "<li>" + "<h3>" + o.class.to_s + "</h3>" + o.to_s.tr('\r\n', '') + "</li>"
+    '<li>' + '<h3>' + o.class.to_s + '</h3>' + o.to_s + '</li>'
   end
 end
 
@@ -54,24 +52,32 @@ end
 class FormatTXT
   def save(objects)
     objects.map { |o|
-      o.class.to_s + " " + o.to_s
-    }
+      o.class.to_s + '\t\t\t' + o.to_s
+    }.join("\n")
   end
 
   def parse(s)
-    s
+    parsed_pairs = []
+    s.each_line { |line|
+      splitted_line = line.split('\t\t\t')
+      parsed_pairs.push([
+        splitted_line[0],
+        splitted_line[1]
+      ])
+    }
+    parsed_pairs
   end
 end
 
 
+format_txt = FormatTXT.new
 format_html = FormatHTML.new
-#format_txt = FormatTXT.new
 
+txt_snapshot = Snapshot.save_state(format_txt)
 html_snapshot = Snapshot.save_state(format_html)
-#txt_snapshot = Snapshot.save_state(format_txt)
 
+File.write('txt_format.txt', txt_snapshot)
+File.write('html_format.html', html_snapshot)
+
+Snapshot.run(format_txt, txt_snapshot)
 Snapshot.run(format_html, html_snapshot)
-#txt_parsed = Snapshot.run(format_txt, txt_snapshot)
-
-#puts "HTML: #{html_parsed}"
-#puts "TXT: #{txt_parsed}"
