cd1048c09daa0bc393a4b8f3fe6269d8a6bc0399
Artur Juraszek <artur@juraszek.xyz>
2018-06-06 20:24:05 +0200

    php: zautomatyzowany generator obciążenia dysku

--- semestr_2_lato_2017_18/php/DatabasePerformance/data-gen.php
+++ semestr_2_lato_2017_18/php/DatabasePerformance/data-gen.php
@@ -4,14 +4,32 @@ require_once("./vendor/autoload.php");
 
 use Nelmio\Alice\Loader\NativeLoader;
 
-$aliceLoader = new NativeLoader();
-
 $howManyTransactionsToCreate = $argv[1];
+$apiString = '<?php'.PHP_EOL.'$howManyTransactionsToCreate='.$howManyTransactionsToCreate.';';
+file_put_contents('superAPI.php', $apiString);
 
-echo 'uuu' . PHP_EOL;
-echo $howManyTransactionsToCreate;
-$ha = 0;
-
+$aliceLoader = new NativeLoader();
 $objectSet = $aliceLoader->loadFile(__DIR__.'/fixtures.php');
 
-var_dump($objectSet);
+echo "koniec generacji!".PHP_EOL;
+
+$pdo = new PDO('pgsql:dbname=DatabasePerformance user=napewnonieadmin password=brak');
+$sqlInsertFakeTransactions = $pdo->prepare(
+    'INSERT INTO transactions (pos_id, date_time, amount, title) VALUES (?, ?, ?, ?);'
+);
+
+$sqlInsertFakeTransactions->bindParam(1, $pos_id);
+$sqlInsertFakeTransactions->bindParam(2, $date_time);
+$sqlInsertFakeTransactions->bindParam(3, $amount);
+$sqlInsertFakeTransactions->bindParam(4, $title);
+
+//$pdo->beginTransaction();
+foreach ($objectSet->getObjects() as $transactionObject)
+{
+    $pos_id = $transactionObject->pos_id;
+    $date_time = $transactionObject->date_time->format('Y-m-d H:i:s');
+    $amount = $transactionObject->amount;
+    $title = $transactionObject->title;
+    $sqlInsertFakeTransactions->execute();
+}
+//$pdo->commit();
--- semestr_2_lato_2017_18/php/DatabasePerformance/fixtures.php
+++ semestr_2_lato_2017_18/php/DatabasePerformance/fixtures.php
@@ -1,17 +1,17 @@
 <?php
 
-echo 'UWAGA';
-echo $ha;
-include('kek.txt');
-echo $zmienna;
+require_once('./vendor/autoload.php');
+
+use Asgavar\DatabasePerformance\Entities\Transaction;
+include('superAPI.php');
 
 return [
-    \Asgavar\DatabasePerformance\Entities\Transaction::class => [
+    Transaction::class => [
         'transaction{1..'.$howManyTransactionsToCreate.'}' => [
             'pos_id' => '<randomNumber()>',
             'date_time' => '<dateTime()>',
-            'amount' => '<numberBetween(0, 9999999)>',  // TODO
-            'title' => '<text()>'
+            'amount' => '<randomFloat()>',
+            'title' => '<sentence()>'
         ]
     ]
 ];
