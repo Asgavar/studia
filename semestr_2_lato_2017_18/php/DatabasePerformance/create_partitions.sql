fd2a3048b298d8d7bf55389c4d0af86ebc3ea8c3
Artur Juraszek <artur@juraszek.xyz>
2018-06-07 19:17:30 +0200

    php: poprawki partycjonowania

--- semestr_2_lato_2017_18/php/DatabasePerformance/create_partitions.sql
+++ semestr_2_lato_2017_18/php/DatabasePerformance/create_partitions.sql
@@ -14,8 +14,7 @@ create table transactions_q4 (
 check (extract(month from date_time) between 10 and 12)
 ) inherits (transactions);
 
-create or replace function transactions_partition_function()
-returns trigger as $$
+create or replace function transactions_partition_function() as $$
 declare
     transaction_month double precision := extract(month from new.date_time);
 begin
@@ -28,7 +27,6 @@ begin
     elsif (transaction_month between 10 and 12) then
         insert into transactions_q4 values (new.*);
     end if;
-    return null;
 end;
 $$
 language plpgsql;
