b05312ee543cd591c9e3b1c321fa1455f6ecc55a
Artur Juraszek <artur@juraszek.xyz>
2018-11-07 10:50:03 +0100

    pf: Lista 04 vol 4

--- semestr_3_zima_2018_19/pf/lista04/zadanie01.ml
+++ semestr_3_zima_2018_19/pf/lista04/zadanie01.ml
@@ -1,15 +1,18 @@
-let rec startswith xs maybe_prefix =
-  match (xs, maybe_prefix) with
-    (xs, []) -> true
-  | ([], maybe_prefix) -> false
-  | (x :: xstl, mp :: maybe_prefixtl) ->
-      if x = mp then startswith xstl maybe_prefixtl else false
+let left_startswith_reversed_right left right =
+  let rec aux left right =
+    match right with
+      [] -> failwith "niemozliwe"
+    | [last] -> ((List.hd left) = last, (List.tl left))
+    | _ ->
+        let further_tuple = aux left (List.tl right) in
+          ((fst further_tuple) && (List.hd (snd further_tuple)) = (List.hd right)), (List.tl (snd further_tuple)) in
+  fst (aux left right)
 
 let is_palindrome xs =
   let rec aux left right =
     match (left, right) with
-      (left, []) -> startswith xs (List.rev left)
-    | (left, [last_elem]) -> startswith xs (List.rev left)
+      (left, []) -> left_startswith_reversed_right xs left
+    | (left, [last_elem]) -> left_startswith_reversed_right xs left
     | (l :: left_tail, r :: r_tail) -> aux left_tail (List.tl r_tail)
     | (_, _) -> failwith "something is wrong" in
   aux xs xs
@@ -19,3 +22,4 @@ let test_is_palindrome = is_palindrome ['a'; 'b'; 'b'; 'a']
 let test_is_palindrome = is_palindrome ['a'; 'b'; 'c'; 'd'; 'c'; 'b'; 'a']
 let test_is_palindrome = is_palindrome [5; 5; 5; 5]
 let test_is_palindrome = is_palindrome [1; 2; 3]
+let test_left_startswith_reversed_right = left_startswith_reversed_right [1; 2; 3; 4] [3; 2; 1]
--- semestr_3_zima_2018_19/pf/lista04/zadanie05.ml
+++ semestr_3_zima_2018_19/pf/lista04/zadanie05.ml
@@ -8,7 +8,19 @@ let prod tree =
         aux ltree (fun so_far -> nodeval * so_far * (aux rtree cont))
   in aux tree (fun x -> x)
 
+let prod_plus tree =
+  let rec aux tree cont =
+    match tree with
+      Leaf -> cont 1
+    | Node (ltree, 0, rtree) -> 0
+    | Node (ltree, nodeval, rtree) ->
+        aux ltree (fun so_far -> nodeval * so_far * (aux rtree cont))
+  in aux tree (fun x -> x)
+
 let test_tree =
   Node (Node (Leaf, 2, Leaf), 4, Node (Leaf, 3, Leaf))
+let test_tree_plus =
+  Node (Node (Leaf, 2, Leaf), 0, Node (Leaf, 3, Leaf))
 
 let test_result = prod test_tree
+let test_result_plus = prod_plus test_tree_plus
