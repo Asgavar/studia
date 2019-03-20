df9db853c8711e4a4c308ac383fed44bced6b43a
Artur Juraszek <artur@juraszek.xyz>
2019-03-20 17:51:22 +0100

    prolog: Finalne poprawki do trzeciej listy

--- semestr_4_lato_2018_19/prolog/lista03.pl
+++ semestr_4_lato_2018_19/prolog/lista03.pl
@@ -44,23 +44,58 @@ concat_number_([DigitsHead|DigitsTail], Num) :-
 decimal_(Num, [Num]) :-
     digit(Num).
 
+filter([], []).
+filter([HNum|TNum], LPos) :-
+    HNum < 0,
+    filter(TNum, LPos).
+filter([HNum|TNum], [HPos|TPos]) :-
+    HNum >= 0,
+    HNum = HPos,
+    filter(TNum, TPos).
+
 %% zadanie 2.
 count(_, [], 0).
-count(Elem, [Elem|Tail], Count) :-
+count(Elem, [Head|Tail], Count) :-
+    unifiable(Elem, Head, _),
     count(Elem, Tail, PrevCount),
     Count is PrevCount + 1, !.
 count(Elem, [_|Tail], Count) :-
     count(Elem, Tail, Count), !.
 
 %% zadanie 3.
-% TODO dokończyć
 mylength([], 0).
 mylength([_|Tail], Length) :-
+    var(Length),
     mylength(Tail, TailLength),
     Length is TailLength + 1.
+mylength([_|Tail], Length) :-
+    PrevLength is Length - 1,
+    mylength(Tail, PrevLength), !.
 
 %% zadanie 5.
 append([LastList], LastList).
 append([HeadList|TailList], Appended) :-
     append(TailList, TailAppended),
     append(HeadList, TailAppended, Appended), !.
+
+%% zadanie 6.
+puzzle(U, S, A, R, P, E, C) :-
+    sublist(AllNums, [0,1,2,3,4,5,6,7,8,9]),
+    permutation(AllNums, [U,S,A,R,P,E,C]),
+    concat_number([U,S,A], Usa),
+    concat_number([U,S,S,R], Ussr),
+    concat_number([P,E,A,C,E], Peace),
+    Peace is Usa + Ussr,
+    U \= 0,
+    P \= 0, !.
+
+%% backport z listy drugiej
+sublist([], _).
+sublist([H|T1], [H|T2]) :-
+    sublist(T1, T2).
+sublist(X, [_|T]) :-
+    sublist(X, T).
+
+%% zadanie 7.
+sum(X, 0, X).
+sum(0, Y, Y).
