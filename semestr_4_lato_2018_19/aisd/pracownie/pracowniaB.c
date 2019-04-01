8b0affbe6c502b66bf252b8d825266f5aba2fec2
Artur Juraszek <artur@juraszek.xyz>
2019-04-01 21:50:47 +0200

    aisd: Kolejka kolejeczka kolejunia

--- semestr_4_lato_2018_19/aisd/pracownie/pracowniaB.c
+++ semestr_4_lato_2018_19/aisd/pracownie/pracowniaB.c
@@ -1,4 +1,8 @@
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SWAP(a, b) a = a ^ b; b = a ^ b; a = b ^ a
 
 /*
  * The two input parameters.
@@ -8,89 +12,92 @@
 int MATRIX_SIZE;
 int FIRST_K;
 
-typedef struct {
-  int row;
-  int column;
-} multiplication_t;
 
-/*
- * Odd indices hold min values, while even ones - max values.
- */
-typedef struct {
-  multiplication_t **vals;
-  long long minimal_value;
-} minmax_heap_t;
+typedef long long multiplication_t;
 
 typedef struct {
-  int idx;
-  multiplication_t * mult;
-} minmax_child;
+  multiplication_t *vals;
+  int last_occupied;
+  int max_size;
+} maxheap_t;
 
-minmax_child descendants[6];
+int parent(int idx) {
+  if (idx % 2 == 1)
+    return idx / 2;
+  return (idx-1) / 2;
+}
 
-minmax_child* children_and_grandchildren(minmax_heap_t *heap, int idx) {
-  // TODO: informować o nie wszystkich potomkach
+void sift_up(maxheap_t *heap, int idx) {
+  int curr = idx;
+  while (heap->vals[curr] > heap->vals[parent(curr)]) {
+    SWAP(heap->vals[curr], heap->vals[parent(curr)]);
+    curr = parent(curr);
+  }
+}
 
-  int son_idx = 2*idx + 1;
-  int daughter_idx = 2*idx + 2;
+void sift_down(maxheap_t *heap, int idx) {
+  int curr = idx;
+  while (2*curr + 2 < heap->max_size) {
+    multiplication_t left_child = heap->vals[2*curr + 1];
+    multiplication_t right_child = heap->vals[2*curr + 2];
 
-  descendants[0].idx = son_idx;
-  descendants[0].mult = heap->vals[son_idx];
-  descendants[1].idx = daughter_idx;
-  descendants[1].mult = heap->vals[daughter_idx];
+    if (heap->vals[curr] > left_child && heap->vals[curr] > right_child)
+      return;
 
-  if (2*idx + 2 <= FIRST_K) {
-    int son_son_idx = 2*son_idx + 1;
-    int son_daughter_idx = 2*son_idx + 2;
-    int daughter_son_idx = 2*daughter_idx + 1;
-    int daughter_daughter_idx = 2*daughter_idx + 2;
+    // we always choose the greater child in order to avoid
+    // breaking the heap property
+    if (left_child > right_child) {
+      SWAP(heap->vals[2*curr + 1], heap->vals[curr]);
+      curr = 2*curr + 1;
+    } else {
+      SWAP(heap->vals[2*curr + 2], heap->vals[curr]);
+      curr = 2*curr + 2;
+    }
+  }
+}
 
-    descendants[2].idx = son_son_idx;
-    descendants[2].mult = heap->vals[son_son_idx];
-    descendants[3].idx = son_daughter_idx;
-    descendants[3].mult = heap->vals[son_daughter_idx];
-    descendants[4].idx = daughter_son_idx;
-    descendants[4].mult = heap->vals[daughter_son_idx];
-    descendants[5].idx = daughter_daughter_idx;
-    descendants[5].mult = heap->vals[daughter_daughter_idx];
+multiplication_t peek_max(maxheap_t *heap) {
+  return heap->vals[0];
+}
+
+void pop_max(maxheap_t *heap) {
+  heap->vals[0] = heap->vals[heap->last_occupied];
+  heap->vals[heap->last_occupied] = -1;
+  --heap->last_occupied;
+  sift_down(heap, 0);
+}
+
+void insert_new(maxheap_t *heap, multiplication_t elem) {
+  heap->vals[++heap->last_occupied] = elem;
+  sift_up(heap, heap->last_occupied);
+}
+
+int main() {
+  maxheap_t heap;
+  heap.max_size = 10;
+  heap.last_occupied = -1;
+
+  multiplication_t arr[10];
+  memset(arr, -1, 10 * sizeof(multiplication_t));
+
+  heap.vals = arr;
+
+  insert_new(&heap, 8);
+  insert_new(&heap, 7);
+  insert_new(&heap, 10);
+  insert_new(&heap, 1);
+  insert_new(&heap, 3);
+  insert_new(&heap, 5);
+  insert_new(&heap, 6);
+  insert_new(&heap, 2);
+  insert_new(&heap, 4);
+  insert_new(&heap, 9);
+
+  for (short idx = 0; idx < 10; idx++) {
+    /* printf("%lld\n", arr[idx]); */
+    printf("%lld\n", peek_max(&heap));
+    pop_max(&heap);
   }
 
-  return descendants;
+  return 0;
 }
-
-/* max-specific ops */
-long long peek_max(minmax_heap_t *heap) {
-  multiplication_t *mult1 = heap->vals[1];
-  multiplication_t *mult2 = heap->vals[3];
-  // TODO: tu chyba będzie niejawna konwersja która wszystko rozjebie
-  return fmax(mult1->row * mult1->column, mult2->row * mult2->column);
-}
-
-void sift_down_min(minmax_heap_t *heap, int idx) {
-  if (2*idx + 1 > FIRST_K)
-    return;
-
-
-}
-
-long long pop_max(minmax_heap_t *heap) { return -1; }
-
-/* min-specific ops */
-long long peek_min(minmax_heap_t *heap) {
-  multiplication_t *mult = heap->vals[0];
-  return mult->row * mult->column;
-}
-
-long long pop_min(minmax_heap_t *heap) { return -1; }
-
-void sift_down_max(minmax_heap_t *heap, int idx) {}
-
-/* generic interface */
-void sift_down(minmax_heap_t *heap, int idx) {
-  if (idx % 2 == 1)
-    sift_down_min(heap, idx);
-  else
-    sift_down_min(heap, idx);
-}
-
-void sift_up(minmax_heap_t *heap) {}
