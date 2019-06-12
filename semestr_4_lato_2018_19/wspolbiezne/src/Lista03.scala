e582d374879cd69b2cc94e8153a1ba5df0ebf732
Artur Juraszek <artur@juraszek.xyz>
2019-06-12 22:03:16 +0200

    wspołbieżne: Kolejeczka

--- semestr_4_lato_2018_19/wspolbiezne/src/Lista03.scala
+++ semestr_4_lato_2018_19/wspolbiezne/src/Lista03.scala
@@ -1,33 +1,66 @@
-class MyQueue[+A](private val foreahead: List[A], private val waitingInReverse: List[A]) {
+class MyQueue[A](val forehead: List[A], val waitingInReverse: List[A]) {
   def this() =
     this(List(), List())
 
   def enqueue(elem: A): MyQueue[A] =
-    new MyQueue(foreahead, elem :: waitingInReverse)
+    this match {
+      case _ if this.forehead.isEmpty =>
+        new MyQueue[A]((elem :: this.waitingInReverse).reverse, Nil)
+      case _ =>
+        new MyQueue[A](this.forehead, elem :: this.waitingInReverse)
+    }
 
-  def first: A =
-    this.maybeDoTheSwitch().foreahead.head
+  def first: A = {
+    if (this.isEmpty)
+      throw new Exception("popping from empty queue")
+    this.forehead.head
+
+  }
 
   def firstOption: Option[A] = {
-    this.maybeDoTheSwitch().foreahead.headOption
+    this.forehead.headOption
   }
 
   def dequeue: MyQueue[A] =
     this match {
-      case queue if queue.foreahead.isEmpty && queue.waitingInReverse.isEmpty => MyQueue.empty
-      case queue if queue.foreahead.isEmpty => new MyQueue(waitingInReverse.reverse, Nil)
-      case queue => new MyQueue(queue.foreahead.tail, waitingInReverse)
+      case _ if this.isEmpty =>
+        MyQueue.empty
+      case _ if this.forehead.length == 1 =>
+        new MyQueue[A](this.waitingInReverse.reverse, Nil)
+      case _ =>
+        new MyQueue[A](this.forehead.tail, this.waitingInReverse)
     }
 
   def isEmpty: Boolean =
-    this.foreahead.isEmpty && this.waitingInReverse.isEmpty
-
-  private def maybeDoTheSwitch(): MyQueue[A] =
-    if (this.foreahead.isEmpty) new MyQueue(waitingInReverse.reverse, Nil) else this
+    this.forehead.isEmpty && this.waitingInReverse.isEmpty
 }
 
 object MyQueue {
   def apply[A](initialElements: A*) = new MyQueue[A](initialElements.toList, Nil)
   def apply = new MyQueue(Nil, Nil)
-  def empty[A]: MyQueue[A] = MyQueue()
+  def empty[A]: MyQueue[A] = new MyQueue[A](Nil, Nil)
+}
+
+object Lista03 extends App {
+  override def main(args: Array[String]): Unit = {
+    val q: MyQueue[Int] = MyQueue.empty
+    var qModified = q.enqueue(1)
+    qModified = qModified.enqueue(2)
+    qModified = qModified.enqueue(3)
+
+    println(q.firstOption)
+    println(qModified.first)
+
+    qModified = qModified.dequeue
+    println(qModified.firstOption)
+
+    qModified = qModified.dequeue
+    println(qModified.firstOption)
+
+    qModified = qModified.dequeue
+    println(qModified.firstOption)
+
+    println(MyQueue.empty.firstOption)
+    println(MyQueue.empty.dequeue.firstOption)
+  }
 }
